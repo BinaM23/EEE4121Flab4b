bfe6f4a2cbae6c4ce599b6388878c862594c1e72
BinaM23 <100226104+BinaM23@users.noreply.github.com>
2023-05-12 23:06:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 
 #loads the 2 python modules l2 learning and firewall.py into the OF controller
 echo "setting up controller and firewall"
-~/pox/pox.py forwarding.l2_learning misc.firewall2 &
+~/pox/pox.py forwarding.l2_learning misc.firewall &
 
 echo "creating network in other window"
 gnome-terminal -x sh -c "cd ~/mininet/lab4/eee4121f-b-lab2-sdn/firewall | sudo python sdn.py; exec bash"
